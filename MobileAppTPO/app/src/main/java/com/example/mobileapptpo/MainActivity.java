9dac674ee864a0f8c1f21274610db5f426c08b8b
Gasper Krevs <gasper.krevs@gmail.com>
2022-12-13 01:17:56 +0100

    mobileAppStart

---
+++ MobileAppTPO/app/src/main/java/com/example/mobileapptpo/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.mobileapptpo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button_scan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button_scan = findViewById(R.id.button_scan);
+        button_scan.setOnClickListener(v ->
+        {
+            //Kličemo scan metodo za skeniranje
+            scanCode();
+        });
+    }
+
+    private void scanCode()
+    {
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to use flash");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+
+        barLauncher.launch(options);
+    }
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result -> {
+
+        if(result.getContents() != null){
+            //Starting a  Intent
+            Intent next= new Intent(getApplicationContext(), MainScreen.class);
+            startActivity(next);
+        }
+    });
+}
