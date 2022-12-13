0dc26e883a1705b3e58845b88b6eee7e87e92c38
Gasper Krevs <gasper.krevs@gmail.com>
2022-12-13 01:21:57 +0100

    n

--- MobileAppTPO/app/src/main/java/com/example/mobileapptpo/MainScreen.java
+++ MobileAppTPO/app/src/main/java/com/example/mobileapptpo/MainScreen.java
@@ -9,6 +9,5 @@ public class MainScreen extends Activity
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
-
     }
 }
