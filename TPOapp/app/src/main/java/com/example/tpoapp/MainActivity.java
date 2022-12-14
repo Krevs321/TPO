f0eea22717c7a3b23cd1086532679e1d09845614
Gasper Krevs <gasper.krevs@gmail.com>
2022-12-14 12:15:34 +0100

    Dodajanje scana na DODAJ NAPRAVO

--- TPOapp/app/src/main/java/com/example/tpoapp/MainActivity.java
+++ TPOapp/app/src/main/java/com/example/tpoapp/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.tpoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button dodaj_napravo_scan;
+    Button dodaj_streznik_scan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dodaj_napravo_scan = findViewById(R.id.dodaj_napravo);
+        dodaj_napravo_scan.setOnClickListener(v ->
+        {
+            //Kličemo scan metodo za skeniranje
+            scanCode();
+        });
     }
+
+    private void scanCode()
+    {
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to use flash");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+
+        barLauncher.launch(options);
+    }
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result -> {
+
+        if(result.getContents() != null){
+            //Starting a  Intent
+            Intent next= new Intent(getApplicationContext(), MainScreen.class);
+            startActivity(next);
+        }
+    });
 }
