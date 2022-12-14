2871a49d4ecaa4a390b7093a4c6aee8cb13ee40c
Gasper Krevs <gasper.krevs@gmail.com>
2022-12-14 17:11:25 +0100

    qwe

--- TPOapp/app/src/main/java/com/example/tpoapp/SeznanjeneNaprave.java
+++ TPOapp/app/src/main/java/com/example/tpoapp/SeznanjeneNaprave.java
@@ -3,22 +3,23 @@ package com.example.tpoapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SeznanjeneNaprave extends AppCompatActivity
 {
-    Button back;
+    ImageButton back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.seznanjenenaprave);
 
-        back = findViewById(R.id.button_b);
+        back = findViewById(R.id.imageButton_b);
 
         back.setOnClickListener(l ->{
-            Intent vse_naprave_intent = new Intent(this, SeznanjeneNaprave.class);
+            Intent vse_naprave_intent = new Intent(this, MainActivity.class);
             startActivity(vse_naprave_intent);
         });
     }
