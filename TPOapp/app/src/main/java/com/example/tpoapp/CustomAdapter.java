7b56341c74774df713436d62866d91569d800dd6
Gasper Krevs <gasper.krevs@gmail.com>
2022-12-21 17:47:36 +0100

    update

--- TPOapp/app/src/main/java/com/example/tpoapp/CustomAdapter.java
+++ TPOapp/app/src/main/java/com/example/tpoapp/CustomAdapter.java
@@ -1,11 +1,9 @@
 package com.example.tpoapp;
 
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
